e6d63f4cac760a5abcd1ab01c0f5706940cd56af
tw-yoo <twyoo99@gmail.com>
2024-11-30 16:14:44 +0900

    resolve secret

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 source venv/bin/activate
 
 # Run the FastAPI app in the background, outputting to a log file
-nohup uvicorn main:app --port 8000 --host 0.0.0.0 > fastapi.log 2>&1 &
+uvicorn main:app --host 0.0.0.0 --port 8000 --ssl-keyfile=key.pem --ssl-certfile=cert.pem > fastapi.log 2>&1 & > fastapi.log 2>&1 &
 
 # Display a message
 echo "Process on port 8000 started."
